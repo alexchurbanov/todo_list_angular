004198b5cbc0704adf3004923fc4d1ce94e83dac
Alexander Churbanov <alexanderxchurbanov@gmail.com>
2022-09-17 20:44:49 +0300

    use async pipe

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,33 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Todo, TodoService } from '../../services/todo.service';
-import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.sass'],
 })
-export class TodoListComponent implements OnInit, OnDestroy {
-  todos!: Todo[];
+export class TodoListComponent {
   todos$: Observable<Todo[]>;
-  onDestroy$: Subject<boolean>;
 
   constructor(private todoService: TodoService) {
     this.todos$ = todoService.todos$;
-    this.onDestroy$ = new Subject<boolean>();
-  }
-
-  ngOnInit(): void {
-    this.todos$
-    .pipe(takeUntil(this.onDestroy$))
-    .subscribe(value => {
-      this.todos = value;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.onDestroy$.next(true);
-    this.onDestroy$.complete();
   }
 }
